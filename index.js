ff6f4bf3bd40280b8aaeeead94243310349022da
R-Srijanki <jankirathod999@gmail.com>
2025-11-16 16:30:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const app=new express();
+mongoose.connect("mongodb+srv://jankirathod999_db_user:H6i46vyt5LaafdJZ@cluster0.pdt2k5e.mongodb.net/");
+const db=mongoose.connection;
+db.on("open",()=>{
+    console.log("connection successful");
+})
+
+db.on("error",()=>{
+    console.log("connection not successful");
+})
+// middlewares that handles both json and urlencoded data
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(8000,()=>{
+    console.log("Server listening on port 8000");
+})
+//jankirathod999_db_user
+//H6i46vyt5LaafdJZ
